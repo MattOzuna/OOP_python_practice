8212c3efb01c78a655629f94d36478fc6cd7b4a3
Matt Ozuna <m.ozuna22@gmail.com>
2023-11-03 10:47:41 -0400

    completed serial generator and wordfinder

--- serial.py
+++ serial.py
@@ -20,3 +20,19 @@ class SerialGenerator:
     100
     """
 
+    def __init__(self, start):
+        '''This creates and inital value and saves the inital value to use for resetting'''
+        
+        self.start = start - 1
+        self.reset_value = start
+    
+    def generate(self):
+        '''Returns a updated serial incrimenting by 1'''
+
+        self.start += 1
+        return self.start
+    
+    def reset(self):
+        '''Resets the initial value to the start + 1'''
+
+        self.start = self.reset_value - 1
--- wordfinder.py
+++ wordfinder.py
@@ -1,5 +1,44 @@
 """Word Finder: finds random words from a dictionary."""
 
+from random import choice
 
 class WordFinder:
-    ...
+    '''
+    >>> wf = WordFinder("words.txt")
+        3 words read
+
+    >>> wf.random()
+        'cat'
+
+    >>> wf.random()
+        'cat'
+
+    >>> wf.random()
+        'porcupine'
+
+    >>> wf.random()
+        'dog'
+    '''
+    
+    def __init__(self, file):
+        self.file = file
+        self.word_list = []
+
+    def word_choice(self):
+        lines = open(self.file).read().splitlines()
+        for num in range(3):
+            line = choice(lines)
+            self.word_list.append(line)
+
+    def random(self):
+        if len(self.word_list) == 0:
+            self.word_choice()
+        
+        return choice(self.word_list)
+
+        
+
+            
+            
+                
+
